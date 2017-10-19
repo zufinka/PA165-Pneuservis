b132ed8bf285526194a4e819aec65be95f932471
Martin Zilak <tinnyx@gmail.com>
2017-10-20 00:32:20 +0200

    started working on the Order entity

--- backend/src/main/java/cz/muni/fi/pa165/pneuservis/backend/entity/Order.java
+++ backend/src/main/java/cz/muni/fi/pa165/pneuservis/backend/entity/Order.java
@@ -1,4 +1,31 @@
 package cz.muni.fi.pa165.pneuservis.backend.entity;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.util.List;
+
+/***
+ * @author Martin Zilak, 433372@mail.muni.cz
+ */
+@Entity
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime date;
+
+    @NotNull
+    @ManyToOne
+    private Customer customer;
+
+    @ManyToMany
+    @JoinTable
+    private List<Tire> tires;
+
+    @ManyToMany
+    @JoinTable
+    private List<Service> services;
 }
