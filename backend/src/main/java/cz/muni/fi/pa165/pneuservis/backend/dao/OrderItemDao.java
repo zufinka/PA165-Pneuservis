68608b3176e4d5f8b62e700898852bd6182ed42b
Martin Zilak <tinnyx@gmail.com>
2017-10-23 00:48:04 +0200

    added javadoc

--- backend/src/main/java/cz/muni/fi/pa165/pneuservis/backend/dao/OrderItemDao.java
+++ backend/src/main/java/cz/muni/fi/pa165/pneuservis/backend/dao/OrderItemDao.java
@@ -2,21 +2,50 @@ package cz.muni.fi.pa165.pneuservis.backend.dao;
 
 import cz.muni.fi.pa165.pneuservis.backend.entity.OrderItem;
 
+import javax.validation.ConstraintViolationException;
 import java.util.List;
 
 /**
+ * Data Access Object specification for the class {@link OrderItem}
  * @author Martin Zilak, 433372@mail.muni.cz
  */
 
 public interface OrderItemDao {
 
+    /**
+     * Retrieves an order item with the specified ID.
+     * @param id of the requested order item.
+     * @return retrieved order item or {@code null}, if there is no order item with the specified ID.
+     */
     OrderItem getById(Long id);
 
+    /**
+     * Retrieves all order items.
+     * @return list of retrieved order items.
+     */
     List<OrderItem> getAll();
 
-    void create(OrderItem order);
+    /**
+     * Stores a new order item in the database.
+     * @param orderItem to be stored.
+     * @throws NullPointerException if the {@code orderItem} is {@code null}.
+     * @throws ConstraintViolationException if {@code orderItem} has {@code null} quantity or the quantity is less than 1.
+     */
+    void create(OrderItem orderItem);
 
-    void update(OrderItem order);
+    /**
+     * Modifies the specified order item in the database.
+     * @param orderItem to be modified.
+     * @throws NullPointerException if the {@code orderItem} is {@code null}.
+     * @throws ConstraintViolationException if {@code orderItem} has {@code null} quantity or the quantity is less than 1.
+     */
+    void update(OrderItem orderItem);
 
-    void delete(OrderItem order);
+    /**
+     * Removes the specified order item from the database.
+     * @param orderItem to be removed.
+     * @throws NullPointerException if the {@code orderItem} is {@code null}.
+     * @throws ConstraintViolationException if {@code orderItem} has {@code null} quantity or the quantity is less than 1.
+     */
+    void delete(OrderItem orderItem);
 }
