720b72583f9c31491d827fa0d125e1a6a4fdc3ec
Martin Zilak <tinnyx@gmail.com>
2017-10-22 22:01:31 +0200

    added OrderItem entity, reworked Order entity

--- backend/src/main/java/cz/muni/fi/pa165/pneuservis/backend/entity/Order.java
+++ backend/src/main/java/cz/muni/fi/pa165/pneuservis/backend/entity/Order.java
@@ -1,11 +1,16 @@
 package cz.muni.fi.pa165.pneuservis.backend.entity;
 
+import com.google.common.base.MoreObjects;
+
 import javax.persistence.*;
+import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 
-/***
+/**
  * @author Martin Zilak, 433372@mail.muni.cz
  */
 @Entity
@@ -15,17 +20,96 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
     private LocalDateTime date;
+
+    @NotNull
+    @DecimalMin("0.0")
+    private BigDecimal priceTotal;
 
     @NotNull
     @ManyToOne
     private Customer customer;
 
-    @ManyToMany
-    @JoinTable
-    private List<Tire> tires;
+    @NotNull
+    @OneToMany
+    private List<OrderItem> orderItems;
 
-    @ManyToMany
-    @JoinTable
-    private List<Service> services;
+    public Order() {
+    }
+
+    public Order(LocalDateTime date, BigDecimal priceTotal, Customer customer, List<OrderItem> orderItems) {
+        this.date = date;
+        this.priceTotal = priceTotal;
+        this.customer = customer;
+        this.orderItems = orderItems;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public BigDecimal getPriceTotal() {
+        return priceTotal;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public List<OrderItem> getOrderItems() {
+        return orderItems;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public void setPriceTotal(BigDecimal priceTotal) {
+        this.priceTotal = priceTotal;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public void setOrderItems(List<OrderItem> orderItems) {
+        this.orderItems = orderItems;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Order)) return false;
+        Order order = (Order) o;
+        return Objects.equals(id, order.id) &&
+                Objects.equals(date, order.date) &&
+                Objects.equals(priceTotal, order.priceTotal) &&
+                Objects.equals(customer, order.customer) &&
+                Objects.equals(orderItems, order.orderItems);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, date, priceTotal, customer, orderItems);
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+                .add("id", id)
+                .add("date", date)
+                .add("priceTotal", priceTotal)
+                .add("customer", customer)
+                .add("orderItems", orderItems)
+                .toString();
+    }
 }
