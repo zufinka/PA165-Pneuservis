88abdbf161aa5075c54aa6669f51f8a82ee9e8c9
Zuzana Žufanová <x393906@fi.muni.cz>
2017-11-26 18:23:45 +0100

    CustomerService trivial methods

---
+++ services/src/main/java/services/CustomerServiceImpl.java
@@ -0,0 +1,62 @@
+package services;
+
+import cz.muni.fi.pa165.pneuservis.backend.dao.CustomerDao;
+import cz.muni.fi.pa165.pneuservis.backend.entity.Customer;
+
+import javax.persistence.EntityExistsException;
+import java.util.List;
+import javax.inject.Inject;
+
+/**
+ * Implementation of service methods for access to {@link Customer} entity
+ * @author Zuzana Žufanová, zufinka@mail.muni.cz
+ */
+public class CustomerServiceImpl implements CustomerService{
+    @Inject
+    CustomerDao customerDao;
+
+    @Override
+    public Customer findById(Long id) {
+        return customerDao.findById(id);
+    }
+
+    @Override
+    public List<Customer> findByName(String name, String surname) {
+        return customerDao.findByName(name, surname);
+    }
+
+    @Override
+    public Customer findByEmail(String email) {
+        return customerDao.findByEmail(email);
+    }
+
+    @Override
+    public Customer findByPhone(String phoneNumber) {
+        return customerDao.findByPhone(phoneNumber);
+    }
+
+    @Override
+    public List<Customer> findByAddress(String city, String street, String country, String zipCode) {
+        return customerDao.findByAddress(city, street, country, zipCode);
+    }
+
+    @Override
+    public List<Customer> findAll() {
+        return customerDao.findAll();
+    }
+
+    @Override
+    public void createCustomer(Customer customer) {
+        customerDao.createCustomer(customer);
+    }
+
+    @Override
+    public void updateCustomer(Customer customer) {
+        customerDao.updateCustomer(customer);
+    }
+
+    @Override
+    public void deleteCustomer(Customer customer) {
+        customerDao.deleteCustomer(customer);
+    }
+}
