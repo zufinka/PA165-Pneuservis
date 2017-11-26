edc4b05005ac4552ec46f9454ee4403c313fba55
Zuzana Žufanová <x393906@fi.muni.cz>
2017-11-26 22:04:26 +0100

    CustomerDTO + small changes

--- api/src/main/java/dto/CustomerDTO.java
+++ api/src/main/java/dto/CustomerDTO.java
@@ -1,7 +1,167 @@
 package dto;
 
+import javax.validation.constraints.NotNull;
+
 /**
- * @author Martin Zilak, 433372@mail.muni.cz
+ * @author Zuzana Žufanová, zufinka@mail.muni.cz
  */
 public class CustomerDTO {
+    private Long id;
+
+    @NotNull
+    private String email;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String surname;
+
+    @NotNull
+    private String city;
+
+    @NotNull
+    private String street;
+
+    @NotNull
+    private String country;
+
+    @NotNull
+    private String zipCode;
+
+    @NotNull
+    private String phoneNumber;
+
+    private boolean vip;
+
+    public CustomerDTO(){}
+
+    public CustomerDTO(String name, String surname, String city, String street, String zipCode, String country,
+                    String email, String phoneNumber){
+        this.name = name;
+        this.surname = surname;
+        this.city = city;
+        this.street = street;
+        this.zipCode = zipCode;
+        this.country = country;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public boolean isVip() {
+        return vip;
+    }
+
+    public void setVip(boolean vip) {
+        this.vip = vip;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CustomerDTO)) return false;
+
+        CustomerDTO that = (CustomerDTO) o;
+
+        if (!getEmail().equals(that.getEmail())) return false;
+        if (!getName().equals(that.getName())) return false;
+        if (!getSurname().equals(that.getSurname())) return false;
+        return getPhoneNumber().equals(that.getPhoneNumber());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getEmail().hashCode();
+        result = 31 * result + getName().hashCode();
+        result = 31 * result + getSurname().hashCode();
+        result = 31 * result + getPhoneNumber().hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomerDTO{" +
+                "email='" + email + '\'' +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", city='" + city + '\'' +
+                ", street='" + street + '\'' +
+                ", country='" + country + '\'' +
+                ", zipCode='" + zipCode + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", vip=" + vip +
+                '}';
+    }
 }
